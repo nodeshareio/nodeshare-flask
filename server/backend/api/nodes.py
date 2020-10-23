1d73c3d78491ba15e079f8151232ce2b5471a1a3
NodeShare.io <admin@nodeshare.io>
2020-10-22 21:30:58 -0400

    Submit Feature
    Node Submit Feature proto implementation

--- server/backend/api/nodes.py
+++ server/backend/api/nodes.py
@@ -11,21 +11,19 @@ from werkzeug.http import HTTP_STATUS_CODES
 @api.route('/submit', methods=['POST'])
 @token_auth.login_required
 def submit_node():
+    print(f"REQUEST DATA {request.data}")
     data = json.loads(request.data) or {'error: no data'}
+    print(f"data: {data}")
     if 'ns_string' not in data:
         return bad_request('must include nodetext')
     if Node.query.filter_by(data=data['ns_string']).first():
         return bad_request('[  ERROR  ]  node text exists')
     print("[  INFO  ]  submit request")  
     print(f"[  NODE TEXT  ]  {data['ns_string']}")  
-    # node = Node()
-    # user.from_dict(data, new_user=True)
-    # db.session.add(user)
-    # db.session.commit()
-
-    payload = {'status': HTTP_STATUS_CODES.get(200, 'Pass')}
-    payload['message'] = "Submit dev"
-    response = jsonify(payload)
+    node = Node()
+    node.from_dict(data, approved=False)
+    db.session.add(node)
+    db.session.commit()
+    response = jsonify(data)
     response.status_code = 201
-    # response.headers['Location'] = url_for('api.get_user', id=user.id)
     return response
